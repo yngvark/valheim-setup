4935174d0a6b0141d6dc40522600beb47fd5a36f
Yngvar Kristiansen <562343+yngvark@users.noreply.github.com>
2021-03-20 11:11:51 +0100

    Environmentalize some variables in run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 : "${VALHEIM_SERVER_PASS:?Needs to be non-empty.}"
+: "${VALHEIM_SERVER_NAME:?Needs to be non-empty.}"
+: "${VALHEIM_WORLD_NAME:?Needs to be non-empty.}"
 
 # Backup cron explanation:
 # Every hour at minute 0. https://crontab.guru/#0_*_*_*_*
 
-
+# IMPORTANT, if importing a save game! WORLD_NAME must match your world name!
+# See similar comment, ctrl+f "Do not forget to" at
+# https://github.com/lloesche/valheim-server-docker#manual-backup
 
 docker run -d \
     --name valheim-server \
@@ -13,16 +17,14 @@ docker run -d \
     -p 80:80 \
     -v $HOME/valheim-server/config:/config \
     -v $HOME/valheim-server/data:/opt/valheim \
-    -e SERVER_NAME="YKServer" \
-    -e WORLD_NAME="Gurgheim" \
+    -e SERVER_NAME=$VALHEIM_SERVER_NAME \
+    -e WORLD_NAME=$VALHEIM_WORLD_NAME \
     -e SERVER_PASS=$VALHEIM_SERVER_PASS \
-    -e SERVER_PUBLIC="false" \
     -e TZ="Europe/Stockholm" \
     -e BACKUPS_CRON="SE" \
     -e BACKUPS_MAX_AGE="10" \
     -e BACKUPS_CRON="0 * * * *" \
     -e STATUS_HTTP="true" \
+    -e SERVER_PUBLIC="true" \
     lloesche/valheim-server
 
-
-#    -e SERVER_PUBLIC="true" \
